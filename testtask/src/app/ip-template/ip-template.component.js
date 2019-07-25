818d063f005e2d8ac71a14935c35d7a3f66123d4
aomelchuk <111suok@gmail.com>
2019-07-26 02:55:06 +0300

    start include product functionality

--- testtask/src/app/ip-template/ip-template.component.js
+++ testtask/src/app/ip-template/ip-template.component.js
@@ -10,10 +10,47 @@ var db_1 = require('../shared/db');
 var IpTemplateComponent = (function () {
     function IpTemplateComponent() {
     }
-    IpTemplateComponent.prototype.toggle = function () {
-    };
     IpTemplateComponent.prototype.ngOnInit = function () {
         this.db = db_1.db;
+        for (var i in this.db) {
+            this.db[i].status = false;
+        }
+        this.preInclude = [];
+        this.include = [];
+    };
+    IpTemplateComponent.prototype.selectProduct = function (product) {
+        var temp = this.db.find(function (x) { return x.sku == product.sku; });
+        this.preInclude.push(temp);
+        product.status = true;
+        console.log(this.preInclude);
+    };
+    IpTemplateComponent.prototype.unselectProduct = function (product) {
+        //let temp = this.preInclude.find( x => x.sku == product.sku );
+        this.preInclude = this.preInclude.filter(function (x) { return x.sku != product.sku; });
+        product.status = false;
+        console.log(this.preInclude);
+    };
+    IpTemplateComponent.prototype.toggle = function (product) {
+        var status;
+        console.log(this.preInclude.find(function (x) { return x.sku == product.sku; }));
+        if (this.preInclude.find(function (x) { return x.sku == product.sku; }) != undefined) {
+            this.unselectProduct(product);
+        }
+        else {
+            this.selectProduct(product);
+        }
+        return status;
+    };
+    IpTemplateComponent.prototype.moveSelected = function (arrayType) {
+        var _this = this;
+        if (arrayType == 'firstArr') {
+            this.include = this.include.concat(this.preInclude);
+            this.include.forEach(function (element) {
+                element.status = false;
+            });
+            this.db = this.db.filter(function (x) { return _this.preInclude.indexOf(x) < 0; });
+            this.preInclude = [];
+        }
     };
     __decorate([
         core_1.Input()
--- testtask/src/app/ip-template/ip-template.component.ts
+++ testtask/src/app/ip-template/ip-template.component.ts
@@ -12,19 +12,64 @@ export class IpTemplateComponent implements OnInit {
   @Input() includeArr:any[];
   @Input() typeArr:string;
 
-
-  toggle() {
-
-  }
-
-
-  constructor() { }
-
   db;
-
+  preInclude;
+  include;
 
   ngOnInit() {
     this.db = db;
+    for (let i in this.db) {
+      this.db[i].status = false;
+    }
+    this.preInclude=[];
+    this.include = [];
   }
+
+
+  selectProduct(product:any) {
+
+      let temp = this.db.find( x => x.sku == product.sku );
+
+      this.preInclude.push(temp);
+      product.status = true;
+      console.log(this.preInclude);
+  }
+  unselectProduct(product:any) {
+    //let temp = this.preInclude.find( x => x.sku == product.sku );
+    this.preInclude = this.preInclude.filter(x => x.sku != product.sku );
+    product.status = false;
+    console.log(this.preInclude);
+  }
+
+  toggle(product:any) {
+    let status:boolean;
+    console.log(this.preInclude.find( x => x.sku == product.sku ));
+    if(this.preInclude.find( x => x.sku == product.sku )!= undefined) {
+      this.unselectProduct(product);
+
+    } else {
+      this.selectProduct(product);
+
+    }
+    return status;
+  }
+
+  moveSelected(arrayType:string) {
+    if(arrayType == 'firstArr') {
+      this.include = this.include.concat(this.preInclude);
+      this.include.forEach(function(element){
+        element.status = false;
+      });
+      this.db = this.db.filter( x => this.preInclude.indexOf(x) < 0 );
+
+      this.preInclude=[];
+
+    }
+
+  }
+
+  constructor() { }
+
+
 
 }
